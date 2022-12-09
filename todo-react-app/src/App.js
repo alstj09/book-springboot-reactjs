8878421e41f46d07b437e12ef580f269fe3a49e6
Kim Minseo <ms0329.kim@gmail.com>
2022-12-09 15:34:05 +0900

    2-2. 프론트엔드 개발 - 서비스 통합
    백엔드와 프론트엔드 통합

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,46 +1,59 @@
-import React from 'react';
-import Todo from './Todo.js';
-import AddTodo from './AddTodo.js';
-import {Paper, List, Container} from '@material-ui/core';
-import './App.css';
+import React from "react";
+import Todo from "./Todo";
+import AddTodo from "./AddTodo.js";
+import { Paper, List, Container } from "@material-ui/core";
+import "./App.css";
+import { call } from "./service/ApiService";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
-    }
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    call("/todo", "GET", null).then((response) =>
+      this.setState({ items: response.data })
+    );
   }
 
   add = (item) => {
-    const thisItems = this.state.items;
-    item.id = "ID-" + thisItems.length;
-    item.done = false;
-    thisItems.push(item);
-    this.setState({items: thisItems});
-    console.log("items : " , this.state.items);
-  }
+    call("/todo", "POST", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
 
   delete = (item) => {
-    const thisItems = this.state.items;
-    console.log("Before Update Items: ", this.state.items)
-    const newItems = thisItems.filter(e => e.id !== item.id);
-    this.setState({items: newItems}, () => {
-      console.log("Update Items : ", this.state.items)
-    })
-  }
+    call("/todo", "DELETE", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
+
+  update = (item) => {
+    call("/todo", "PUT", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
 
   render() {
     var todoItems = this.state.items.length > 0 && (
-      <Paper style={{margin: 16}}>
+      <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} delete={this.delete} />
+            <Todo
+              item={item}
+              key={item.id}
+              delete={this.delete}
+              update={this.update}
+            />
           ))}
         </List>
       </Paper>
     );
 
+    // 3. props로 넘겨주기
     return (
       <div className="App">
         <Container maxWidth="md">
@@ -48,7 +61,7 @@ class App extends React.Component {
           <div className="TodoList">{todoItems}</div>
         </Container>
       </div>
-      );
+    );
   }
 }
 
